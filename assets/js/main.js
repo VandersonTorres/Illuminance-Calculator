ddd1ba2c40277fcd884775b528708499420dfe7f
Vanderson <vanderson.torres1@hotmail.com>
2023-03-14 12:35:12 -0300

    ajuste

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,6 +306,7 @@ form.addEventListener('submit', (e) => {
 selectIluminacao.addEventListener('click', (e) => {
     e.preventDefault();
 
+    aviso.innerHTML = "";
     const ambiente = selectAmbiente.value;
     let iluminacao = selectIluminacao.value;
     const mensagem = [
@@ -318,32 +319,26 @@ selectIluminacao.addEventListener('click', (e) => {
     ];
 
     if (iluminacao === "geral") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[0];
         aviso.appendChild(span);
     } else if (ambiente === "sala" && iluminacao === "local") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[1];
         aviso.appendChild(span);
     } else if (ambiente === "cozinha" && iluminacao === "local") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[2];
         aviso.appendChild(span);
     } else if (ambiente === "quarto" && iluminacao === "local") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[3];
         aviso.appendChild(span);
     } else if (ambiente === "banheiro" && iluminacao === "local") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[4];
         aviso.appendChild(span);
     } else if (ambiente === "hall-escada-garagem" && iluminacao === "local") {
-        aviso.classList.remove("hide");
         const span = document.createElement("span");
         span.innerHTML = mensagem[5];
         aviso.appendChild(span);
